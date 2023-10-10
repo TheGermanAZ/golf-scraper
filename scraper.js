9ec189b72315077ce905fcd8eba1bdc5185b72a8
german <germanalvarez14@gmail.com>
2023-10-09 20:34:47 -0700

    replace user agents

--- scraper.js
+++ scraper.js
@@ -1,13 +1,13 @@
 const playwright = require("playwright");
-const userAgent = require("user-agents");
+const userAgent = require("random-useragent");
 
 const URL = "https://www.taylormadegolf.com/taylormade-clubs/?lang=en_US";
 
 (async () => {
-  const agent = new userAgent().random();
+  const agent = userAgent.getRandom();
 
   const browser = await playwright.chromium.launch({ headless: false });
-  const context = await browser.newContext({ userAgent: agent.toString() });
+  const context = await browser.newContext({ userAgent: agent });
   const page = await context.newPage({ bypassCSP: true });
   await page.setDefaultTimeout(30000);
   await page.setViewportSize({ width: 800, height: 200 });
